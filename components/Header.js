3c324c6479e7cac54d7a096cbf82bfdd5af60a20
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-04-11 21:26:58 +0530

    search form

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import { router, useRouter } from 'next/router';
+import { useRef } from "react";
 
 function Header() {
     const router =useRouter();
+    const searchInputRef = useRef(null);
     return (
       <header>
         <Image
@@ -12,6 +14,10 @@ function Header() {
         onClick =  {() =>router.push("/")}
         className = "cursor-pointer"        
         />
+
+        <form className = "flex px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
+          <input ref = {searchInputRef} className="flex-grow w-full focu:outline-none" type="text" />
+        </form>
       </header>
     )
 }
