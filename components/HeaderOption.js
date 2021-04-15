e0ed9bb29c95a6700cc40d4e298927ab4cdba0f0
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-04-15 16:18:19 +0530

    header search

--- components/HeaderOption.js
+++ components/HeaderOption.js
@@ -1,8 +1,8 @@
 function HeaderOption({ Icon , title , selected }) {
     return (
-        <div className={`{flex}`}>
-        <Icon className = "h-4" />
-            
+        <div className={`flex items-center space-x-1 border-b-2 border-transparent hover:text-blue-500 hover:border-blue-500 pb-2 cursor-pointer ${selected && "text-blue-500 border-blue-500"}`}>
+        <Icon className = "h-4 text-gray-500" />
+        <p className = "hidden  text-gray-500 sm:inline">{title}</p>
         </div>
     )
 }
