c0a2f3ec3eb119d424f2e21f78a62ef3318e6d73
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-04-15 13:24:22 +0530

    icons

---
+++ components/HeaderOption.js
@@ -0,0 +1,10 @@
+function HeaderOption({ Icon , title , selected }) {
+    return (
+        <div className={`{flex}`}>
+        <Icon className = "h-4" />
+            
+        </div>
+    )
+}
+
+export default HeaderOption;
--- components/HeaderOptions.js
+++ components/HeaderOptions.js
@@ -1,9 +1,31 @@
+import HeaderOption from "./HeaderOption";
+import { 
+    DotsVerticalIcon, 
+    NewspaperIcon, 
+    MapIcon, 
+    PhotographIcon,
+    PlayIcon,
+    SearchIcon
+} from "@heroicons/react/outline";
+
 function HeaderOptions() {
     return (
         <div>
 
             <div className="flex space-x-6">
-
+                <HeaderOption Icon={SearchIcon} 
+                title="All" selected/>
+                <HeaderOption Icon={PhotographIcon} 
+                title="Images" />
+                <HeaderOption Icon={PlayIcon} 
+                title="Videos" />
+                <HeaderOption Icon={NewspaperIcon} 
+                title="News" />
+                <HeaderOption Icon={MapIcon} 
+                title="Map" />
+                <HeaderOption Icon={DotsVerticalIcon} 
+                title="More" />
+                
             </div>
             
             <div className="flex space-x-4">
