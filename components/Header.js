013a8f60e9800de4fb70b99cde416f64627000a1
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-04-16 20:19:29 +0530

    default value

--- components/Header.js
+++ components/Header.js
@@ -31,7 +31,9 @@ function Header() {
         />
 
         <form className = "flex flex-grow px-6 py-3 ml-10 mr-5 border border-gray-200 rounded-full shadow-lg max-w-3xl items-center">
-          <input ref = {searchInputRef} className="flex-grow w-full focu:outline-none" type="text" />
+          <input ref = {searchInputRef} className="flex-grow w-full focu:outline-none" type="text"
+          defaultValue={router.query.term}
+          />
 
           <XIcon className="h-6 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125" 
           onClick={() => (searchInputRef.current.value = "")}
