e2bf024974edd9749ade997ee0c393eaef8363bd
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-04-16 19:39:19 +0530

    lised search

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,7 +1,22 @@
 function SearchResults({ results }) {
     return (
-        <div>
-            
+        <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
+            <p className = "text-gray-600 text-md mb-5 mt-3">About {results.searchInformation?.formattedTotalResults} results ({results.searchInformation?.formattedSearchTime} seconds)
+            </p>
+
+            {results.items?.map((result) =>(
+                <div key={result.link} className = "max-w-xl mb-8"> 
+
+                <div> 
+                    <a href={result.link} className="text-sm">{result.formattedUrl}</a>
+                    <a href={result.link} className="text-sm">
+                        <h2 className="truncate text-xl text-blue-800 font-medium">{result.title}</h2>
+                    </a>
+                </div>
+                <p>{result.snippet}</p>
+                </div>
+            ))}
+
         </div>
     )
 }
