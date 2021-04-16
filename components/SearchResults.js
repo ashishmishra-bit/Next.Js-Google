3cf591f497eac36b5979c9752860415399da9f09
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-04-16 20:08:47 +0530

    completed

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,3 +1,7 @@
+import PaginationButtons from "./PaginationButtons";
+
+
+
 function SearchResults({ results }) {
     return (
         <div className="mx-auto w-full px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
@@ -7,18 +11,19 @@ function SearchResults({ results }) {
             {results.items?.map((result) =>(
                 <div key={result.link} className = "max-w-xl mb-8"> 
 
-                <div> 
+                <div className = "group"> 
                     <a href={result.link} className="text-sm">{result.formattedUrl}</a>
                     <a href={result.link} className="text-sm">
-                        <h2 className="truncate text-xl text-blue-800 font-medium">{result.title}</h2>
+                        <h2 className="truncate text-xl text-blue-800 font-medium group-hover:underline">{result.title}</h2>
                     </a>
                 </div>
-                <p>{result.snippet}</p>
+                <p className = "line-clamp-2">{result.snippet}</p>
                 </div>
             ))}
-
+            
+                <PaginationButtons />
         </div>
-    )
+    );
 }
 
 export default SearchResults
